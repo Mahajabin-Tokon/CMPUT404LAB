40af4b68bc9c083c1739d41df8c09b7eb5604a7b
Mahajabin Tokon <tokonislam1122@gmail.com>
2022-09-12 19:03:28 -0600

    Code changed

--- firstLabScript.py
+++ firstLabScript.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.get("http://www.google.com/").text)
+print(requests.get("https://raw.githubusercontent.com/Mahajabin-Tokon/CMPUT404LAB/main/firstLabScript.py").text)
